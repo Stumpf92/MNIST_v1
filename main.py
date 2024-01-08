9ae427d69249d810c09719604fb138b2c236f005
Stefan Stumpf <stefan.stumpf92@googlemail.com>
2024-01-08 21:13:50 +0100

    1

--- main.py
+++ main.py
@@ -18,19 +18,20 @@ model = Model(train_dataset, test_dataset)
 solution = 123456789
 
 
-def shuffle():
-    random_row = test_dataset.iloc[row]
-    i = 0
-    complete_row = []
-    while i <= 28**2-1:
-        complete_row.append(random_row.iloc[i]) 
-        i += 1
+# def shuffle(row):
+#     random_row = test_dataset.iloc[row]
 
-    grid = np.reshape(complete_row,(28,28))
-    grid = np.transpose(grid)
-    label = solution
+#     i = 0
+#     complete_row = []
+#     while i <= 28**2-1:
+#         complete_row.append(random_row.iloc[i]) 
+#         i += 1
 
-    return grid, label
+#     grid = np.reshape(complete_row,(28,28))
+#     grid = np.transpose(grid)
+#     label = solution
+
+#     return grid, label
 
 
 pg.init()
@@ -38,9 +39,9 @@ clock = pg.time.Clock()
 screen = pg.display.set_mode((WIDTH*PIXEL_SIZE,HEIGTH*PIXEL_SIZE))
 pg.display.set_caption('zufälliges Bild aus MNIST')
 
-row = random.randint(1,len(train_dataset))
-grid = shuffle()[0]
-label = shuffle()[1]
+# row = random.randint(1,len(train_dataset))
+# grid = shuffle(row)[0]
+# label = shuffle(row)[1]
 
 
 
@@ -51,15 +52,15 @@ while run:
     clock.tick(FPS)
     screen.fill((0,255,0))
 
-    for i in range(WIDTH):
-        for j in range(HEIGTH):
-            pg.draw.rect(screen, (grid[i][j],grid[i][j],grid[i][j]), pg.Rect(i*PIXEL_SIZE, j*PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE))
+    # for i in range(WIDTH):
+    #     for j in range(HEIGTH):
+    #         pg.draw.rect(screen, (grid[i][j],grid[i][j],grid[i][j]), pg.Rect(i*PIXEL_SIZE, j*PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE))
     
-    font = pg.font.SysFont(None, 24)
-    img_row = font.render("Reihe: "+str(row), True, (255,0,0))
-    screen.blit(img_row, (10, 10))
-    img_label = font.render("Label: "+str(label), True, (255,0,0))
-    screen.blit(img_label, (10, 25))
+    # font = pg.font.SysFont(None, 24)
+    # img_row = font.render("Reihe: "+str(row), True, (255,0,0))
+    # screen.blit(img_row, (10, 10))
+    # img_label = font.render("Label: "+str(label), True, (255,0,0))
+    # screen.blit(img_label, (10, 25))
 
 
     for event in pg.event.get():
@@ -68,10 +69,10 @@ while run:
         elif event.type == pg.KEYDOWN:
             if event.key == pg.K_ESCAPE:
                 run = False
-            if event.key == pg.K_SPACE:
-                row += 1
-                grid = shuffle()[0]
-                label = shuffle()[1]             
+            # if event.key == pg.K_SPACE:
+            #     row += 1
+            #     grid = shuffle(row)[0]
+            #     label = shuffle(row)[1]             
 
 
     pg.display.flip()
